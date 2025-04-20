9d19665dc25f714d42f7a1b666f18e7230d9efbd
Anthony Taranto <anthony.taranto@gmail.com>
2025-04-19 23:41:37 -0700

    use Result.map

--- src/Domino/Game.fs
+++ src/Domino/Game.fs
@@ -7,8 +7,4 @@ module Game =
     /// Creates a new game with the given players.
     /// Returns the initial sequence of events.
     let start (rules: Rules) players =
-        let result = rules.start players
-
-        match result with
-        | Ok _ -> Ok { Foo = 3 }
-        | Error error -> Error result
+        players |> rules.start |> Result.map (fun _ -> { Foo = 3 })
