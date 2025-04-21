32eee6ab24f289c0372f79da742024340c58f0ba
Anthony Taranto <anthony.taranto@gmail.com>
2025-04-20 23:22:58 -0700

    use Result functions in tests

--- src/Domino.Test/Game.fs
+++ src/Domino.Test/Game.fs
@@ -4,7 +4,6 @@ open Xunit
 open FsUnit.Xunit
 
 open Domino
-open Domino.Game
 
 // Add tests for the Game module here
 [<Fact>]
@@ -14,20 +13,13 @@ let ``placeholder test`` () = true |> should be True
 let ``Game.start should initialize the game state correctly`` () =
     let rules = Domino.SimpleRules()
 
-    let players = [ { Name = "Alice"; Id = 0 }; { Name = "Bob"; Id = 1 } ]
-    let result = Game.start rules players
-
-    match result with
-    | Ok state -> state |> should equal { Foo = 3 }
-    | Error err -> failwithf "Unexpected error: %A" err
+    [ { Id = 0; Name = "Alice" }; { Id = 1; Name = "Bob" } ]
+    |> Game.start rules
+    |> Result.contains { Foo = 3 }
+    |> should be True
 
 
 [<Fact>]
 let ``Game.start should fail if the players list is empty`` () =
     let rules = Domino.SimpleRules()
-    let players = [] // Empty players list
-    let result = Game.start rules players
-
-    match result with
-    | Error _ -> ()
-    | _ -> failwith "Expected a Result.Error but got a Result.Ok"
+    [] |> Game.start rules |> Result.isError |> should be True
--- src/Domino/Rules.fs
+++ src/Domino/Rules.fs
@@ -5,7 +5,7 @@ type Rules =
 
 type SimpleRules() =
     interface Rules with
-        member this.start players =
+        member _.start players =
             if List.length players >= 2 && List.length players <= 4 then
                 Ok()
             else
