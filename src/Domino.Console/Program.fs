faddb780e9d132c813becd765814ac24479564c7
Anthony Taranto <anthony.taranto@gmail.com>
2025-08-05 23:58:56 -0700

    better prompt run

--- src/Domino.Console/Program.fs
+++ src/Domino.Console/Program.fs
@@ -4,60 +4,53 @@ open Domino.SimpleRules
 let players = [ { Id = 0; Name = "Player #0" }; { Id = 1; Name = "Player #1" } ]
 let rules = Impl() :> Rules<State, Action>
 
-// let rec prompt state =
-//     printfn ""
-//     let player = state.Active
-//     let actions = state |> rules.actions
+let rec prompt (playing: Playing) =
+    printfn ""
+    let player = playing.Active
+    let actions = (Playing playing) |> rules.actions
 
-//     actions
-//     |> List.indexed
-//     |> List.iter (fun (index, (action)) -> printfn "[%d] [%02d] %A" index 0 action)
+    actions
+    |> List.indexed
+    |> List.iter (fun (index, (action)) -> printfn "[%d] [%02d] %A" index 0 action)
 
-//     printf "%s Action: " player.Name
+    printf "%s Action: " player.Name
 
-//     match System.Console.ReadLine() |> System.Int32.TryParse with
-//     | true, c when c < actions.Length -> actions |> List.item c
-//     | _ -> state |> prompt
+    match System.Console.ReadLine() |> System.Int32.TryParse with
+    | true, c when c < actions.Length -> actions |> List.item c
+    | _ -> playing |> prompt
 
-// let rec run state =
-//     printfn "%A" state.Board
-//     printfn ""
+let rec run (playing: Playing) =
+    printfn "%A" playing.Board
+    printfn ""
 
-//     state.Players
-//     |> Map.iter (fun player ps ->
-//         let tiles =
-//             ps.Tiles
-//             |> List.sortByDescending weight
-//             |> List.map (function
-//                 | Tile(x, y) -> sprintf "[%d %d] " x y)
-//             |> System.String.Concat
+    playing.Players
+    |> Map.iter (fun player ps ->
+        let tiles =
+            ps.Tiles
+            |> List.sortByDescending weight
+            |> List.map (function
+                | Tile(x, y) -> sprintf "[%d %d] " x y)
+            |> System.String.Concat
 
-//         printfn "[%03d] %s %s" ps.Score player.Name tiles)
+        printfn "[%03d] %s %s" ps.Score player.Name tiles)
 
-//     state
-//     |> prompt
-//     |> fun action -> state |> rules.play action
-//     |> function
-//         | Error err -> printfn "Error: %A" err
-//         | Ok state' -> run state'
+    playing
+    |> prompt
+    |> fun action -> (Playing playing) |> rules.play action
+    |> function
+        | Error err -> printfn "Error: %A" err
+        | Ok state' ->
+            match state' with
+            | Playing playing' -> run playing'
+            | Finished -> printfn "Game Finished"
+            | _ -> printfn "Unexpected state"
 
-
-let startGame (state: State) =
-    match rules.play StartGame state with
-    | Ok state -> state
-    | Error err ->
-        printfn "Error: %A" err
-        state
-
-let rec run state = printfn "%A" state
-// let actions = rules.actions state
-// let action = actions |> List.head
-// match rules.play action state with
-// | Ok state -> run state
-// | Error err -> printfn "Error: %A" err
 
 players
 |> rules.start
 |> function
-    | Ok state -> run state
+    | Ok state ->
+        match state with
+        | Playing playing -> run playing
+        | _ -> printfn "Unexpected state after starting game"
     | Error err -> printfn "Error: %A" err
